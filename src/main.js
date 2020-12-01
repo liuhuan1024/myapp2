541f79f1b4a9d961a81005c20a1b9a4934355bd5
liuhuan1024 <liuhuan1024@qq.com>
2020-12-01 11:44:18 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '@/assets/styles/global.scss'
+
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+import 'amfe-flexible'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
